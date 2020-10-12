1decc3b325005069983453ccbf7f443ff5596b22
Tintin <tanner@schenck.xyz>
2020-10-12 19:59:21 -0400

    employees are now in dbeavs

---
+++ ReadMe.md
@@ -0,0 +1,9 @@
+Challenge is to architect and build a solution for managing a company's employees 
+    
+using node, inquirer, and MySQL
+
+Create a CMS the above. 
+
+Start by creating the schema of database
+
+build a solution for managing a company's employees using node, inquirer, and MySQL
---
+++ db/connection.js
@@ -0,0 +1,20 @@
+//require dependencies
+var mysql = require("mysql");
+var inquirer = require("inquirer");
+
+
+// create the connection information for the sql database
+var connection = mysql.createConnection({
+    host: "localhost",
+
+    // Your port; if not 3306
+    port: 3306,
+
+    // Your username
+    user: "root",
+
+    // Your password
+    password: "Clive1804",
+    database: "employees_DB"
+});
+
