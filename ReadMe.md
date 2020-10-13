e3c742ca997518e25e6d94821e6b669373c2274f
Tintin <tanner@schenck.xyz>
2020-10-12 22:04:39 -0400

    Start Screen

--- ReadMe.md
+++
@@ -1,9 +0,0 @@
-Challenge is to architect and build a solution for managing a company's employees 
-    
-using node, inquirer, and MySQL
-
-Create a CMS the above. 
-
-Start by creating the schema of database
-
-build a solution for managing a company's employees using node, inquirer, and MySQL
--- db/connection.js
+++ db/connection.js
@@ -14,7 +14,66 @@ var connection = mysql.createConnection({
     user: "root",
 
     // Your password
-    password: "Clive1804",
-    database: "employees_DB"
+    password: "C!ive.1804",
+    database: "employees"
 });
 
+connection.connect(function (err) {
+    if (err) throw err;
+    console.log("connected as id " + connection.threadId + "\n/");
+    startScreen();
+});
+//User will be prompted to with a question of what department they would like to choose. 
+//What the user will first see once logged into node
+//Prompt user w questions 
+// NPM start to... prompt user w questions. 
+function startScreen() {
+    inquirer
+        .prompt({
+            type: "list",
+            choices: [
+                "Add department",
+                "Add role",
+                "Add employee",
+                "View departments",
+                "View roles",
+                "View employees",
+                "Update employee role",
+                "Quit"
+            ],
+            message: "What would you like to do?",
+            name: "option"
+        })
+        .then(function (result) {
+            console.log("You entered: " + result.option);
+
+            switch (result.option) {
+                case "Add department":
+                    addDepartment();
+                    break;
+                case "Add role":
+                    addRole();
+                    break;
+                case "Add employee":
+                    addEmployee();
+                    break;
+                case "View departments":
+                    viewDepartment();
+                    break;
+                case "View roles":
+                    viewRoles();
+                    break;
+                case "View employees":
+                    viewEmployees();
+                    break;
+                case "Update employee role":
+                    updateEmployee();
+                    break;
+                default:
+                    quit();
+            }
+        });
+}
+
+
+ 
